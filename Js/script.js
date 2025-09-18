61b4d739ec81aa063a8e2b1be36ba283b58f3072
Md. Mahmudul Hasan Arif <arifmahmudulhasan84@gmail.com>
2025-09-19 00:48:44 +0600

    homepage done

---
+++ Js/script.js
@@ -0,0 +1,24 @@
+  flatpickr("#datepicker", {
+    dateFormat: "Y-m-d",   
+    allowInput: true,      
+    defaultDate: null,     
+    minDate: "today"       
+  });
+
+
+
+
+  const tp = flatpickr("#timepicker", {
+    enableTime: true,      
+    noCalendar: true,       
+    dateFormat: "h:i K",    
+    time_24hr: false,       
+    defaultDate: null,
+    onReady: function(selectedDates, dateStr, instance) {
+      instance.input.setAttribute('placeholder', 'Select time');
+    }
+  });
+
+  document.getElementById("clockIcon").addEventListener("click", function() {
+    tp.open();
+  });
